31b15e6b642a357da88ce1def5dff1f3a8aa73dd
liwangpei <wangpei.li@hanshow.com>
2021-05-11 17:12:49 +0800

    update

--- VirtualTable/main.cpp
+++ VirtualTable/main.cpp
@@ -6,48 +6,39 @@ typedef void(*Fun)(void);
 
 class Base
 {
- public:
-        virtual void f() { cout << "Base::f" << endl; }
-        virtual void g() { cout << "Base::g" << endl; }
-        virtual void h() { cout << "Base::h" << endl; }
+public:
+    virtual void f() { cout << "Base::f" << endl; }
+    virtual void g() { cout << "Base::g" << endl; }
+    virtual void h() { cout << "Base::h" << endl; }
+
+private:
+    virtual void baseTest() { cout << "Base::baseTest" << endl; }
+
 };
 
 class Base1
 {
- public:
-        virtual void f() { cout << "Base1::f" << endl; }
-        virtual void g() { cout << "Base1::g" << endl; }
-        virtual void h() { cout << "Base1::h" << endl; }
+public:
+    virtual void f() { cout << "Base1::f" << endl; }
+    virtual void g() { cout << "Base1::g" << endl; }
+    virtual void h() { cout << "Base1::h" << endl; }
 };
 
 class Driver : public Base, public Base1
 {
- public:
-        virtual void f1() { cout << "Driver::f1" << endl; }
-        virtual void g1() { cout << "Driver::g1" << endl; }
-        virtual void h1() { cout << "Driver::h1" << endl; }
-        virtual void f() { cout << "Driver::f" << endl; }
-        virtual void g() { cout << "Driver::g" << endl; }
-        virtual void h() { cout << "Driver::h" << endl; }
+public:
+    virtual void f1() { cout << "Driver::f1" << endl; }
+    virtual void g1() { cout << "Driver::g1" << endl; }
+    virtual void h1() { cout << "Driver::h1" << endl; }
+    virtual void f() { cout << "Driver::f" << endl; }
+    virtual void g() { cout << "Driver::g" << endl; }
+    virtual void h() { cout << "Driver::h" << endl; }
 };
 
 typedef void(Base::*Fun1)();
 
 int main()
 {
-    int a = 1;
-    int c = 2;
-    int &p = a;
-    int *ptr = &a;
-
-    printf("p: %ld, %d\n", &p, p);
-    printf("ptr: %ld, %d\n", ptr, *ptr);
-
-    p = c;
-    ptr = &c;
-    printf("p: %ld, %d\n", &p, p);
-    printf("ptr: %ld, %d\n", ptr, *ptr);
-
     #if 0
     Base *b = new Base();
     cout << "Class object addr: " << b << endl;
@@ -94,23 +85,50 @@ int main()
 
     Fun pFun = NULL;
 
-    Driver *pD = new Driver;
-    cout << "Class object addr: " << (int*)pD << endl;
 
+    Base *pB = new Base();
+    cout << "Base object addr: " << (int*)pB << endl;
+    cout << "1st V-Table addr: " << (int*)*(int*)pB << endl;
+    int fBaddr = *((int*)*(int*)pB + 0*sizeof(int*)/4);
+    int gBaddr = *((int*)*(int*)pB + 1*sizeof(int*)/4);
+    int hBaddr = *((int*)*(int*)pB + 2*sizeof(int*)/4);
+    int tBaddr = *((int*)*(int*)pB + 3*sizeof(int*)/4);
+    cout << "V-Table with 1st V-Fun addr: " << (int*)fBaddr << endl;
+    cout << "V-Table with 2st V-Fun addr: " << (int*)gBaddr << endl;
+    cout << "V-Table with 3st V-Fun addr: " << (int*)hBaddr << endl;
+    cout << "V-Table with 4st V-Fun addr: " << (int*)tBaddr << endl;
+
+    pFun = (Fun)fBaddr;
+    pFun();
+
+    pFun = (Fun)gBaddr;
+    pFun();
+
+    pFun = (Fun)hBaddr;
+    pFun();
+
+    pFun = (Fun)tBaddr;
+    pFun();
+
+    Driver *pD = new Driver;
+    cout << "\nDriver object addr: " << (int*)pD << endl;
     cout << "1st V-Table addr: " << (int*)pD << endl;
     int faddr = *((int*)*(int*)pD + 0*sizeof(int*)/4);
     int gaddr = *((int*)*(int*)pD + 1*sizeof(int*)/4);
     int haddr = *((int*)*(int*)pD + 2*sizeof(int*)/4);
-    int f1addr = *((int*)*(int*)pD + 3*sizeof(int*)/4);
-    int g1addr = *((int*)*(int*)pD + 4*sizeof(int*)/4);
-    int h1addr = *((int*)*(int*)pD + 5*sizeof(int*)/4);
+    int taddr = *((int*)*(int*)pD + 3*sizeof(int*)/4);
+    int f1addr = *((int*)*(int*)pD + 4*sizeof(int*)/4);
+    int g1addr = *((int*)*(int*)pD + 5*sizeof(int*)/4);
+    int h1addr = *((int*)*(int*)pD + 6*sizeof(int*)/4);
 
     cout << "V-Table with 1st V-Fun addr: " << (int*)faddr << endl;
     cout << "V-Table with 2st V-Fun addr: " << (int*)gaddr << endl;
     cout << "V-Table with 3st V-Fun addr: " << (int*)haddr << endl;
-    cout << "V-Table with 4st V-Fun addr: " << (int*)f1addr << endl;
-    cout << "V-Table with 5st V-Fun addr: " << (int*)g1addr << endl;
-    cout << "V-Table with 6st V-Fun addr: " << (int*)h1addr << endl;
+    cout << "V-Table with 4st V-Fun addr: " << (int*)taddr << endl;
+    cout << "V-Table with 5st V-Fun addr: " << (int*)f1addr << endl;
+    cout << "V-Table with 6st V-Fun addr: " << (int*)g1addr << endl;
+    cout << "V-Table with 7st V-Fun addr: " << (int*)h1addr << endl;
+
     pFun = (Fun)faddr;
     pFun();
 
@@ -118,6 +136,10 @@ int main()
     pFun();
 
     pFun = (Fun)haddr;
+    pFun();
+
+    //is unsafe, the private virtual function of base class is traversed through pointer.
+    pFun = (Fun)taddr;
     pFun();
 
     pFun = (Fun)f1addr;
@@ -129,7 +151,7 @@ int main()
     pFun = (Fun)h1addr;
     pFun();
 
-    cout << "2st V-Table addr: " << (int*)pD + 2 << endl;
+    cout << "\n2st V-Table addr: " << (int*)pD + 2 << endl;
 
     faddr = *((int*)*(int*)((int*)pD + 2) + 0*sizeof(int*)/4);
     gaddr = *((int*)*(int*)((int*)pD + 2) + 1*sizeof(int*)/4);
@@ -138,7 +160,6 @@ int main()
     cout << "V-Table with 1st V-Fun addr: " << (int*)faddr << endl;
     cout << "V-Table with 2st V-Fun addr: " << (int*)gaddr << endl;
     cout << "V-Table with 3st V-Fun addr: " << (int*)haddr << endl;
-
 
     pFun = (Fun)faddr;
     pFun();
